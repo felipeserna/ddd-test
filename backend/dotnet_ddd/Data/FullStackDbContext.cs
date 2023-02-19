88bebee83ac8d1ec3d1ec0a917693d576725ca74
Felipe Serna <feserna86@gmail.com>
2023-02-18 21:10:21 -0500

    1st time CRUD is working

---
+++ backend/dotnet_ddd/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dotnet_ddd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_ddd.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
