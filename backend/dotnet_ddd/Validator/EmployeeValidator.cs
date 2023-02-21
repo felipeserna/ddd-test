6302ef99584e3c14f9f44b6b5fba664d835009aa
Felipe Serna <feserna86@gmail.com>
2023-02-21 18:30:23 -0500

    Adding chained validation rules for 'Name' and 'Department'. Backend

--- backend/dotnet_ddd/Validator/EmployeeValidator.cs
+++ backend/dotnet_ddd/Validator/EmployeeValidator.cs
@@ -7,11 +7,11 @@ namespace dotnet_ddd.Validator
     {
         public EmployeeValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty");
+            RuleFor(x => x.Name).NotEmpty().Must(x => x.ToString().Length >= 4).WithMessage("Name must be at least 4 characters long.");
             RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");
             RuleFor(x => x.Phone).Must(x => x.ToString().Length == 7).WithMessage("Phone must have 7 digits");
             RuleFor(x => x.Salary).GreaterThan(10000).WithMessage("Salary must be greater than $10000");
-            RuleFor(x => x.Department).NotEmpty().WithMessage("Department can not be empty");
+            RuleFor(x => x.Department).NotEmpty().Must(x => x.ToString().Length >= 4).WithMessage("Department must be at least 4 characters long.");
         }
 
     }
